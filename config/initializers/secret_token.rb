6e49c70ff63e8bb9745f983f70c0c498449e8d83
Brandon Cordell <brandon@brandoncordell.com>
2013-02-05 16:58:12 -0500

    Generate new Rails 3.2.11 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClosetCrunch::Application.config.secret_token = 'f61901c36f332bad5d0e70b07502695b5137b026e098c2fc669436e31f24b738bc869410137ae78777e58b68dd976ac90b02f4bc4d7df5b56a5cdc68b8a75836'
