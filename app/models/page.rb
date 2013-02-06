1aa62885b066df09307a0d8a3fc7b94d1f864db4
Brandon Cordell <brandon@brandoncordell.com>
2013-02-06 03:02:28 -0500

    Add Page#published scope

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,8 @@
 class Page < ActiveRecord::Base
   attr_accessible :content, :description, :keywords, :published, :slug, :title
 
+  scope :published, -> { where published: true }
+
   validates :title, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
   validates :content, presence: true
