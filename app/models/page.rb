b7833dc4b159efc8ced24103aaa0cc7f3288e9cd
Brandon Cordell <brandon@brandoncordell.com>
2013-02-06 02:03:30 -0500

    Add some basic validation to the Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,7 @@
 class Page < ActiveRecord::Base
   attr_accessible :content, :description, :keywords, :published, :slug, :title
+
+  validates :title, presence: true, uniqueness: true
+  validates :slug, presence: true, uniqueness: true
+  validates :content, presence: true
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Page do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_uniqueness_of :title }
+  it { should validate_presence_of :slug }
+  it { should validate_uniqueness_of :slug }
+  it { should validate_presence_of :content }
 end
