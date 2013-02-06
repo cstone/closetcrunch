c114d4f926250839502e71bbd62a5ef8ac636aed
Brandon Cordell <brandon@brandoncordell.com>
2013-02-06 17:40:56 -0500

    Add custom routes that for devise so you don't have to prefix with /users/ to login, or signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 ClosetCrunch::Application.routes.draw do
   devise_for :users
+  as :user do
+    get 'signup' => 'devise/registrations#new', as: :new_user_registration
+    post 'signup' => 'devise/registrations#create', as: :user_registration
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   resources :pages, only: :show
   get '/:id' => 'pages#show'
