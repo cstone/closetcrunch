6cc3b372d10e658046ff1af3ac48db4ac17d6091
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 17:37:50 -0500

    Add a User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+salt = 'asdasdastr4325234324sdfds'
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email) { |n| "user#{n}@example.com" }
+    salt { salt }
+    crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("mypassword", salt) }
   end
 end
