6d32e4af873d9fc24828c3a34e87282a515119bd
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 18:56:15 -0500

    Update the users factory with a logged_in_user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,21 @@
-salt = 'asdasdastr4325234324sdfds'
+password_salt = 'asdasdastr4325234324sdfds'
 
 FactoryGirl.define do
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
-    salt { salt }
-    crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("mypassword", salt) }
+    email
+    password 'mypassword'
+    password_confirmation 'mypassword'
+  end
+
+  factory :logged_in_user, class: :user do
+    email
+    password 'secret123'
+    password_confirmation 'secret123'
+    salt password_salt
+    crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("secret123", password_salt) }
   end
 end
