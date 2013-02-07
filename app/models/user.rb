ff2062c61d181e015569471089370c9d9001b506
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 18:56:25 -0500

    Add basic User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :email, :password, :password_confirmation
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true, presence: true
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should validate_presence_of :password }
+  it { should validate_confirmation_of :password }
+  it { should validate_presence_of :password_confirmation }
 end
