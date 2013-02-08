bb9d7df12c1049f66ccc4742cdda8be5cd49d3c4
Brandon Cordell <brandon@brandoncordell.com>
2013-02-08 13:52:14 -0500

    Add validation for acceptance of agreement and privacy policy on registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
 
   ActionView::Base.field_error_proc = Proc.new do |html, instance|
     if html =~ /label/
-      %(<div class="error">#{html}</div>).html_safe
+      if html =~ /agreement_to_terms/
+        %(<div class="error">#{html}<p class="help-block">#{instance.error_message.first}</p></div>).html_safe
+      else
+        %(<div class="error">#{html}</div>).html_safe
+      end
     else
-      %(<div class="error">#{html}<p class="help-block">#{instance.error_message.first}</p></div>).html_safe
+      if html =~ /user_agree/
+        %(<div class="error">#{html}</div>).html_safe
+      else
+        %(<div class="error">#{html}<p class="help-block">#{instance.error_message.first}</p></div>).html_safe
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :email, :password, :salt, :crypted_password
+  attr_accessible :email, :password, :salt, :crypted_password, :agree
+
+  scope :recent, -> { order 'id desc' }
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :agree, acceptance: true
 end
