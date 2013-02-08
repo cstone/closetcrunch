8f2a8d5cad98e4b36bf9b768ebc52050e636e536
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 21:04:44 -0500

    Add /join routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 ClosetCrunch::Application.routes.draw do
   resources :users, except: :index
-
   resources :pages, only: :show
+
+  get '/join' => 'users#new', as: :new_user_registration
+
   get '/:id' => 'pages#show'
 
   root to: 'pages#show', id: :home
