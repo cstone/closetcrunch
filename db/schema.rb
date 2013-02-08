05fa77df23232c6f0536d87ff1e4b9116ffa93e9
Brandon Cordell <brandon@brandoncordell.com>
2013-02-08 11:58:21 -0500

    Add user_activation module to Sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208052617) do
+ActiveRecord::Schema.define(:version => 20130208165518) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -60,11 +60,16 @@ ActiveRecord::Schema.define(:version => 20130208052617) do
   add_index "pages", ["slug"], :name => "index_pages_on_slug", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",            :null => false
+    t.string   "email",                       :null => false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+    t.string   "activation_state"
+    t.string   "activation_token"
+    t.datetime "activation_token_expires_at"
   end
+
+  add_index "users", ["activation_token"], :name => "index_users_on_activation_token"
 
 end
