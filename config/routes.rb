b12b99f3fa0f4bbdc7120054d03736e5d086f2e7
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 21:12:27 -0500

    Add login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 ClosetCrunch::Application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: :index
   resources :pages, only: :show
 
   get '/join' => 'users#new', as: :new_user_registration
+  get '/login' => 'sessions#new', as: :new_user_session
+  post '/login' => 'sessions#create', as: :create_user_session
+  delete '/logout' => 'sessions#destroy', as: :destroy_user_session
 
   get '/:id' => 'pages#show'
 
