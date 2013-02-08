c4d6f53433acc69eccff2b06d7dd190741e457f2
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 21:04:04 -0500

    Add UsersController#new and UsersController#create logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new params[:user]
+    if @user.save
+      redirect_to root_path, flash: { success: 'Thanks for joining Closetcrunch' }
+    else
+      render :new
+    end
   end
 
   def edit
