77912aadcadea09c1533c7fe8c3837e6cb0e0762
Brandon Cordell <brandon@brandoncordell.com>
2013-02-08 18:55:50 -0500

    I guess we don't need that monkey patch anymore thanks to the new version of Sorcery!

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,4 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates :agree, acceptance: true
-
-  protected
-
-  # Protected: Authenticate a User without checking if they've activated their account
-  #
-  # credentials - The same splat of credentials that would be passed into Sorcery's
-  #               login() method
-  #
-  # Examples
-  #
-  #   User.authenticate_without_active_check(params[:email], params[:password])
-  #
-  # Returns an instance of User
-  def self.authenticate_without_active_check(*credentials)
-    prevent_check = self.sorcery_config.before_authenticate.delete :prevent_non_active_login
-    user = self.authenticate *credentials
-    self.sorcery_config.before_authenticate << prevent_check if prevent_check
-    return user
-  end
 end
