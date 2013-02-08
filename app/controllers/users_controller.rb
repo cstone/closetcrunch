4709cc3cbf186b74a3696756edd322eaa241ceab
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 21:08:31 -0500

    Add the rest of the UsersController logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = find_user
   end
 
   def update
+    @user = find_user
+    if @user.update_attributes params[:user]
+      redirect_to root_path, flash: { success: 'Your profile has been saved' }
+    else
+      render :edit
+    end
   end
 
   def destroy
+    user = find_user
+    user.delete
+    redirect_to root_path, flash: { success: 'Your account has been disabled' }
+  end
+
+  protected
+
+  def find_user
+    User.find params[:id]
   end
 end
