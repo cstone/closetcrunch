0a86130a4eb3c46c2d6c5a8002807fdb4ac0c957
Brandon Cordell <brandon@brandoncordell.com>
2013-02-08 00:29:22 -0500

    Add activeadmin gem
    Generate install and User resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ClosetCrunch::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: :index
   resources :pages, only: :show
