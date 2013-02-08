5f06119f699a0bbfa98540af150b801c72903415
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 21:03:27 -0500

    Remove password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :salt, :crypted_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, confirmation: true, presence: true
-  validates :password_confirmation, presence: true
+  validates :password, presence: true
 end
