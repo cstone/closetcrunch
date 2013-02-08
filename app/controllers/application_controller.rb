e06083c93b9491528e87c5dfeca64788a8719dab
Brandon Cordell <brandon@brandoncordell.com>
2013-02-07 21:02:33 -0500

    Override ActionView::Base.field_error_proc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  ActionView::Base.field_error_proc = Proc.new do |html, instance|
+    if html =~ /label/
+      %(<div class="error">#{html}</div>).html_safe
+    else
+      %(<div class="error">#{html}<p class="help-block">#{instance.error_message.first}</p></div>).html_safe
+    end
+  end
 end
