3639fd998ef14419ab5af0be281eead88d05d0ef
Brandon Cordell <brandon@brandoncordell.com>
2013-02-12 17:16:16 -0500

    Add the reset_password module to Sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209000543) do
+ActiveRecord::Schema.define(:version => 20130212221438) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -68,19 +68,23 @@ ActiveRecord::Schema.define(:version => 20130209000543) do
   add_index "pages", ["slug"], :name => "index_pages_on_slug", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",                        :null => false
+    t.string   "email",                           :null => false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
   end
 
   add_index "users", ["activation_token"], :name => "index_users_on_activation_token"
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
 
 end
